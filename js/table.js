41b943a8bc049f559b56f37ca1c598ccfcacad31
jumashafara <jumashafara0@gmail.com>
2024-04-08 12:03:05 +0300

    initialized project

---
+++ js/table.js
@@ -0,0 +1,21 @@
+const dataidea_url = "https://backend.dataidea.org";
+
+// Function to fetch and insert navbar and footer content
+const fetchHeaderAndFooter = () => {
+  // Fetch header content
+  fetch("header.html")
+    .then((response) => response.text())
+    .then(
+      (html) => (document.querySelector(".header-container").innerHTML = html)
+    );
+
+  // Fetch footer content
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then(
+      (html) => (document.querySelector(".footer-container").innerHTML = html)
+    );
+};
+
+// Call the function to fetch header and footer content
+fetchHeaderAndFooter();
